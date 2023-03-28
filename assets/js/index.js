f19a0b7f73d7d51d11d3de0b79ec66cf3b111433
stanleycubo <tic1apfperniciano@gmail.com>
2023-03-28 12:13:41 +0200

    mod generali

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,21 @@
-let item = document.querySelector(".Hamburger");
-item.addEventListener("click", function (params) {
-    document.body.classList.toggle("hMenuOpen");
-});
-
 // elements
-var elements_to_watch = document.querySelectorAll('.base');
+var elements_to_watch = document.querySelectorAll(".base");
 
-// callback 
-var callback = function(items){
+// callback
+var callback = function (items) {
   items.forEach((item) => {
-    if(item.isIntersecting){
+    if (item.isIntersecting) {
       item.target.classList.add("in-page");
-    } else{
+    } else {
       item.target.classList.remove("in-page");
     }
   });
-}
+};
 
 // observer
-var observer = new IntersectionObserver(callback, { threshold: 0.6 } );
+var observer = new IntersectionObserver(callback, { threshold: 0.6 });
 
 // apply
 elements_to_watch.forEach((element) => {
-  observer.observe(element); 
+  observer.observe(element);
 });
