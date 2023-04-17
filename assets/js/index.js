6924d5c07dab5a8da99f77a1f237968a71448a87
stanleycubo <tic1apfperniciano@gmail.com>
2023-04-17 16:13:03 +0200

    mod a dev pt

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,26 @@ const incrementButton = document.getElementById("buttonRight");
 const decrementButton = document.getElementById("buttonLeft");
 const resetButton = document.getElementById("resetButton");
 
+// elements
+var elements_to_watch = document.querySelectorAll(".base");
+// callback
+var callback = function (items) {
+  items.forEach((item) => {
+    if (item.isIntersecting) {
+      item.target.classList.add("in-page");
+    } else {
+      item.target.classList.remove("in-page");
+    }
+  });
+};
+// observer
+var observer = new IntersectionObserver(callback, { threshold: 0.6 });
+
+// apply
+elements_to_watch.forEach((element) => {
+  observer.observe(element);
+});
+
 //* x counter
 
 let count = 0;
@@ -24,22 +44,3 @@ resetButton.addEventListener("click", () => {
 });
 
 //* fine counter
-// elements
-var elements_to_watch = document.querySelectorAll(".base");
-// callback
-var callback = function (items) {
-  items.forEach((item) => {
-    if (item.isIntersecting) {
-      item.target.classList.add("in-page");
-    } else {
-      item.target.classList.remove("in-page");
-    }
-  });
-};
-// observer
-var observer = new IntersectionObserver(callback, { threshold: 0.6 });
-
-// apply
-elements_to_watch.forEach((element) => {
-  observer.observe(element);
-});
