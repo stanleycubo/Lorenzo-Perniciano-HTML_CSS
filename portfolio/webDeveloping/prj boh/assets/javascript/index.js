4e57643bc6e7326162fe604ee92dacc6603534da
stanleycubo <tic1apfperniciano@gmail.com>
2023-04-17 13:24:37 +0200

    UPDATE pt dev

--- portfolio/webDeveloping/prj boh/assets/javascript/index.js
+++ portfolio/webDeveloping/prj boh/assets/javascript/index.js
@@ -3,8 +3,6 @@ const incrementButton = document.getElementById("buttonRight");
 const decrementButton = document.getElementById("buttonLeft");
 const resetButton = document.getElementById("resetButton");
 
-//* x counter
-
 let count = 0;
 counter.textContent = count;
 
@@ -21,25 +19,4 @@ decrementButton.addEventListener("click", () => {
 resetButton.addEventListener("click", () => {
   count = 0;
   counter.textContent = count;
-});
-
-//* fine counter
-// elements
-var elements_to_watch = document.querySelectorAll(".base");
-// callback
-var callback = function (items) {
-  items.forEach((item) => {
-    if (item.isIntersecting) {
-      item.target.classList.add("in-page");
-    } else {
-      item.target.classList.remove("in-page");
-    }
-  });
-};
-// observer
-var observer = new IntersectionObserver(callback, { threshold: 0.6 });
-
-// apply
-elements_to_watch.forEach((element) => {
-  observer.observe(element);
 });
